fe9a5b5648e94e7061d6cf7f38ec7fe3a54a4cc9
William Ferreira <williamfrs_33@hotmail.com>
2020-08-20 11:24:53 -0300

    comit build

---
+++ build/precache-manifest.0ab9224c750eb146f784e60cd08892a7.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba00e0290adcd1db4c2c4c816f3191fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91c8c123389e117e968e",
+    "url": "/static/css/main.759faa88.chunk.css"
+  },
+  {
+    "revision": "24350ad811bd9d56013b",
+    "url": "/static/js/2.f6211995.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f6211995.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91c8c123389e117e968e",
+    "url": "/static/js/main.26a54540.chunk.js"
+  },
+  {
+    "revision": "f666f20005e6749aae5f",
+    "url": "/static/js/runtime-main.612f7005.js"
+  },
+  {
+    "revision": "723c35894cc502ec0b56948f1b424689",
+    "url": "/static/media/banner.723c3589.png"
+  },
+  {
+    "revision": "f1ef13d370a477b5c8633acdedb00330",
+    "url": "/static/media/bgMenu.f1ef13d3.png"
+  },
+  {
+    "revision": "95c422d0450b1b5d10a3a9dda41ecbb1",
+    "url": "/static/media/cardCell_01.95c422d0.png"
+  },
+  {
+    "revision": "90adfefb9e49e266d27ff70e4f39e835",
+    "url": "/static/media/cardPc_02.90adfefb.png"
+  },
+  {
+    "revision": "b99dfe42f6a482b3c6191a99e6616ab5",
+    "url": "/static/media/cardTv_03.b99dfe42.png"
+  },
+  {
+    "revision": "5e66f66f4509e33d600d13a3d4bac5a1",
+    "url": "/static/media/card_01.5e66f66f.png"
+  },
+  {
+    "revision": "3cffaf9dec270019d670ac9b771ac5f3",
+    "url": "/static/media/card_02.3cffaf9d.png"
+  },
+  {
+    "revision": "49f672de1ecd3795a0d02e146acb1acb",
+    "url": "/static/media/card_03.49f672de.png"
+  },
+  {
+    "revision": "438a532f2b50b63da9fa22bbf99e3245",
+    "url": "/static/media/headerEmail.438a532f.png"
+  },
+  {
+    "revision": "8d887176dbcfa49d044f867f3be8712d",
+    "url": "/static/media/headerFacebook.8d887176.png"
+  },
+  {
+    "revision": "9926a2dc81c16c14b8575237522cb885",
+    "url": "/static/media/headerFotos.9926a2dc.png"
+  },
+  {
+    "revision": "a80de5349ca04914afdee59aab6e5785",
+    "url": "/static/media/headerInstagram.a80de534.png"
+  },
+  {
+    "revision": "b68c4581a401d31096a467961349e8a7",
+    "url": "/static/media/headerInternet.b68c4581.png"
+  },
+  {
+    "revision": "7636d48aa3d928e734a6b57e983608b5",
+    "url": "/static/media/headerPlaystore.7636d48a.png"
+  },
+  {
+    "revision": "93670c52a926528009d7bf9393c54bfc",
+    "url": "/static/media/headerWhatsapp.93670c52.png"
+  }
+]);
